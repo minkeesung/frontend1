142fb00e3aa0afd2c96b8b1b55c895082aaf5add
minkee sung <minkeesung@minkees-MacBook-Air.local>
2019-06-27 19:50:13 -0400

    added project form

---
+++ src/components/project_form/project_form.js
@@ -0,0 +1,224 @@
+import React, { Component } from 'react'
+import BasicsForm from './basics_form'
+import RewardsForm from './rewards_form'
+import { merge, values } from 'lodash'
+
+class ProjectForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formType: "basics",
+      title: "",
+      description: "",
+      end_date: "",
+      funding_goal: 0,
+      details: "",
+      category_id: 0,
+      rewardsNums: [1],
+      rewards: {
+        1: {title: "", description: "", cost: 0, delivery_date: ""}
+      },
+      imageFile: null,
+      imageUrl: null
+    };
+
+    this.updateFormType = this.updateFormType.bind(this);
+    this.updateBasics = this.updateBasics.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateReward = this.updateReward.bind(this);
+    this.renderBasicsButton = this.renderBasicsButton.bind(this);
+    this.renderRewardsButton = this.renderRewardsButton.bind(this);
+    this.renderErrors = this.renderErrors.bind(this);
+    this.updateFile = this.updateFile.bind(this);
+  }
+
+  componentDidMount() {
+    document.body.scrollTop = 0;
+    if (this.props.fetchProject) this.props.fetchProject(this.props.match.params.id);
+    this.props.fetchCategories();
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.project !== newProps.project) {
+      let rewardsNums = [];
+      for (let i = 1; i <= newProps.rewards.length; i++) {
+        rewardsNums.push(i);
+      }
+
+      let rewards = {};
+      newProps.rewards.forEach((reward, i) => {
+        rewards[i + 1] = {
+          title: reward.title,
+          description: reward.description,
+          cost: reward.cost,
+          delivery_date: reward.delivery_date,
+          id: reward.id
+        };
+      });
+
+      let newState = merge({}, this.state);
+      newState.title = newProps.project.title;
+      newState.description = newProps.project.description;
+      newState.end_date = newProps.project.end_date;
+      newState.funding_goal = newProps.project.funding_goal;
+      newState.details = newProps.project.details;
+      newState.category_id = newProps.project.category_id;
+      newState.rewardsNums = rewardsNums;
+      newState.rewards = rewards;
+      newState.imageUrl = newProps.project.image_url;
+
+      this.setState(newState);
+    }
+
+  }
+
+  componentWillUnmount() {
+    this.props.clearErrors();
+  }
+
+  updateFormType(type) {
+    return () => {
+      this.setState({ formType: type });
+    };
+  }
+
+  updateBasics(field) {
+    return e => this.setState({ [field]: e.currentTarget.value });
+  }
+
+  updateReward(rewardNum, field) {
+    if (this.state.rewardsNums.includes(rewardNum)) {
+      return e => {
+        this.setState({rewards: merge({}, this.state.rewards, {[rewardNum]: {[field]: e.currentTarget.value}})});
+      };
+    } else {
+      let rewardsNums = this.state.rewardsNums.slice();
+      rewardsNums.push(rewardNum);
+      const newRewards = merge({}, this.state.rewards, {[rewardNum]: {title: "", description: "", cost: 0, delivery_date: ""}});
+      this.setState({rewardsNums, rewards: newRewards});
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("project[title]", this.state.title);
+    formData.append("project[description]", this.state.description);
+    formData.append("project[end_date]", this.state.end_date);
+    formData.append("project[funding_goal]", this.state.funding_goal);
+    formData.append("project[details]", this.state.details);
+    formData.append("project[category_id]", this.state.category_id);
+    formData.append("project[rewards_attributes]", JSON.stringify(values(this.state.rewards)));
+    formData.append("user[session_token]", this.props.session)
+
+    if (this.state.imageFile) {
+      formData.append("project[image]", this.state.imageFile);
+    }
+    if (this.props.project) {
+      this.props.updateProject(this.props.project.id, formData)
+        .then(data => this.props.history.push(`/projects/${data.project.id}`));
+    } else {
+      this.props.createProject(formData)
+      .then(data => {
+        if (!!data.project) this.props.history.push(`/projects/${data.project.id}`)});
+    }
+  }
+
+  updateFile(e) {
+    let file = e.currentTarget.files[0];
+    let fileReader = new FileReader();
+    fileReader.onloadend = function() {
+      this.setState({ imageFile: file, imageUrl: fileReader.result });
+    }.bind(this);
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  renderBasicsButton() {
+    if (this.state.formType === "basics") {
+      return (
+        <button className="active-button" onClick={this.updateFormType("basics")}>
+          <i className="fa fa-check-circle"></i>
+          &nbsp;&nbsp; Basics
+        </button>
+      );
+    } else {
+      return (
+        <button className="inactive-button" onClick={this.updateFormType("basics")}>
+          <i className="fa fa-check-circle"></i>
+          &nbsp;&nbsp; Basics
+        </button>
+      );
+    }
+  }
+
+  renderRewardsButton() {
+    if (this.state.formType === "rewards") {
+      return (
+        <button className="active-button" onClick={this.updateFormType("rewards")}>
+          <i className="fa fa-check-circle"></i>
+          &nbsp;&nbsp; Rewards
+        </button>
+      );
+    } else {
+      return (
+        <button className="inactive-button" onClick={this.updateFormType("rewards")}>
+          <i className="fa fa-check-circle"></i>
+          &nbsp;&nbsp; Rewards
+        </button>
+      );
+    }
+  }
+
+  renderErrors() {
+    if (this.props.errors) {
+      return (
+        <ul className="project-errors">
+          {this.props.errors.map((error, i) => (
+            <li key={`error-${i}`}>
+              {error}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+	}
+
+  render() {
+    return (
+        <section className="project-form">
+          <ul className="project-form-nav">
+            <li className="basics-button">
+              {this.renderBasicsButton()}
+            </li>
+            <li className="rewards-button">
+              {this.renderRewardsButton()}
+            </li>
+          </ul>
+          <BasicsForm
+            categories={this.props.categories}
+            handleSubmit={this.handleSubmit}
+            formType={this.state.formType}
+            updateBasics={this.updateBasics}
+            state={this.state}
+            renderErrors={this.renderErrors}
+            updateFile={this.updateFile}
+          />
+          <RewardsForm
+            formType={this.state.formType}
+            handleSubmit={this.handleSubmit}
+            updateReward={this.updateReward}
+            state={this.state}
+            renderErrors={this.renderErrors}
+          />
+        </section>
+    )
+  }
+}
+
+
+
+export default ProjectForm
---
+++ src/components/project_form/rewards_form.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import RewardsFormItem from './rewards_form_item'
+import '../../css/rewards_form.scss'
+import '../../css/project_form.scss'
+
+class RewardsForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(e) {
+    e.preventDefault();
+    let rewardNum = this.props.state.rewardsNums.length + 1;
+    this.props.updateReward(rewardNum);
+  }
+
+  render() {
+    if (this.props.formType === "rewards") {
+      return (
+        <section className="rewards-form-container">
+          <div className="rewards-form-copy">
+            <h1>Add rewards!</h1>
+            <p>Invite backers to be a part of the creative experience by offering
+              rewards like a copy of what you’re making, a special experience,
+              or a behind-the-scenes look into your process.</p>
+          </div>
+          <ul className="rewards-forms">
+            {this.props.renderErrors()}
+            {this.props.state.rewardsNums.map(
+              num => <RewardsFormItem key={num} rewardNum={num} updateReward={this.props.updateReward} state={this.props.state} />
+            )}
+            <button onClick={this.handleAdd} className="add-reward-button">
+              <i className="fa fa-plus"></i>&nbsp;&nbsp;
+              Add a new reward
+            </button>
+            <button onClick={this.props.handleSubmit} className="save-project-button">Save project</button>
+          </ul>
+        </section>
+      );
+    } else {
+      return <p></p>;
+    }
+  }
+}
+
+export default RewardsForm;
