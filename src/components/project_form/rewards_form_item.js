9c70150486177b818c1a3117e2f4a7a82ea5bc51
minkee sung <minkeesung@minkees-MacBook-Air.local>
2019-08-27 20:47:44 -0400

    rewards form

---
+++ src/components/project_form/rewards_form_item.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class RewardsFormItem extends Component {
+  render() {
+    const rewardNum = this.props.rewardNum;
+
+    return (
+      <li className="rewards-form-item">
+        <label className="reward-number">Reward #{this.props.rewardNum}</label>
+        <form>
+          <div className="project-rewards-field">
+            <label className="reward-form-label">Title</label>
+            <input
+              type="text"
+              value={this.props.state.rewards[rewardNum].title}
+              onChange={this.props.updateReward(rewardNum, "title")}
+              />
+          </div>
+          <div className="project-rewards-field">
+            <label className="reward-form-label">Pledge amount</label>
+            <input
+              type="number"
+              value={this.props.state.rewards[rewardNum].cost}
+              onChange={this.props.updateReward(rewardNum, "cost")}
+              placeholder="$ 0"
+              />
+          </div>
+          <div className="project-rewards-field">
+            <label className="reward-form-label">Description</label>
+            <input
+              type="textarea"
+              value={this.props.state.rewards[rewardNum].description}
+              onChange={this.props.updateReward(rewardNum, "description")}
+              />
+          </div>
+          <div className="project-rewards-field">
+            <label className="reward-form-label">Estimated delivery</label>
+            <input
+              type="date"
+              value={this.props.state.rewards[rewardNum].delivery_date}
+              onChange={this.props.updateReward(rewardNum, "delivery_date")}
+              />
+          </div>
+        </form>
+      </li>
+    );
+  }
+}
+
+export default RewardsFormItem;
